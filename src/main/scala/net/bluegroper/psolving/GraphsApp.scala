2f34766edd14bd14516eb6348d445dd69134344e
fede <fede.lopez@gmail.com>
2014-08-18 22:18:29 +1000

    Paint edges

--- src/main/scala/net/bluegroper/psolving/GraphsApp.scala
+++ src/main/scala/net/bluegroper/psolving/GraphsApp.scala
@@ -5,8 +5,7 @@ package net.bluegroper.psolving
  */
 
 import swing._
-import java.awt.RenderingHints
-import scala.swing.Color
+import java.awt.{Color, RenderingHints}
 
 object GraphsApp extends SimpleSwingApplication {
 
@@ -22,30 +21,40 @@ object GraphsApp extends SimpleSwingApplication {
 
       override def paintComponent(g: Graphics2D) = {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-        g.setColor(new Color(100, 100, 100))
+        g.setColor(Color.darkGray)
         g.drawString("Select a start node.", 10, 20)
-
-        graph.nodes.foreach((node: Node) => drawNode(node, g))
         graph.edges.foreach((edge: Edge) => drawEdge(edge, g))
+        graph.nodes.foreach((node: Node) => drawNode(node, g))
       }
     }
   }
 
   def drawNode(n: Node, g: Graphics2D) = {
+    g.setColor(Color.lightGray)
     val xs: Int = top.preferredSize.width / 7
     val x: Int = n.x * xs
     val centre: Int = x + (xs / 2)
     val y: Int = (n.y * xs) + (xs + xs / 4) / 2
 
+    g.fillArc(centre - xs / 4, y, 50, 50, 0, 360)
+
+    g.setColor(Color.darkGray)
     g.drawArc(centre - xs / 4, y, 50, 50, 0, 360)
 
     val width = g.getFontMetrics.stringWidth(n.name)
-
     g.drawString(n.name, centre - (width / 2), y - 10)
   }
 
   def drawEdge(e: Edge, g: Graphics2D) = {
+    val xs: Int = top.preferredSize.width / 7
 
+    val nodeA_x: Int = e.nodeA.x * xs + 50
+    val nodeA_y: Int = (e.nodeA.y * xs) + (xs + xs / 4) / 2
+
+    val nodeB_x: Int = e.nodeB.x * xs + 50
+    val nodeB_y: Int = (e.nodeB.y * xs) + (xs + xs / 4) / 2
+
+    g.drawLine(nodeA_x, nodeA_y + 25, nodeB_x, nodeB_y + 25)
   }
 
 
