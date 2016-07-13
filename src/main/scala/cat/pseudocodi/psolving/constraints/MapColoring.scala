cfcafe1a3a719877ae8fe3a7280755512e76b539
Fede Lopez <fede.lopez@gmail.com>
2016-07-13 23:46:06 +1000

    added flood fill implementation using a loop for the west and east directions as an optimization to avoid the overhead of stack or queue management => not very fast

--- src/main/scala/cat/pseudocodi/psolving/constraints/MapColoring.scala
+++ src/main/scala/cat/pseudocodi/psolving/constraints/MapColoring.scala
@@ -18,8 +18,8 @@ import javax.swing.{ImageIcon, JFrame, JLabel}
   */
 object MapColoring {
 
-  val target: Int = new Color(149, 149, 149).getRGB
-  val yellowRGB: Int = Color.YELLOW.getRGB
+  val targetColor: Int = new Color(149, 149, 149).getRGB
+  val replacementColor: Int = Color.YELLOW.getRGB
 
   def main(args: Array[String]) {
     val file: String = MapColoring.getClass.getResource("vall-daran.png").getFile
@@ -48,14 +48,14 @@ object MapColoring {
   /**
     * Flood fill using recursion
     *
-    * -Xss4M
+    * cons: needs -Xss4M
     *
     * https://en.wikipedia.org/wiki/Flood_fill
     */
   def floodFillRecursive(p: Point, bi: BufferedImage) {
     val color: Int = bi.getRGB(p.x, p.y)
-    if (color == target) {
-      bi.setRGB(p.x, p.y, yellowRGB)
+    if (color == targetColor) {
+      bi.setRGB(p.x, p.y, replacementColor)
       floodFillRecursive(new Point(p.x - 1, p.y), bi)
       floodFillRecursive(new Point(p.x + 1, p.y), bi)
       floodFillRecursive(new Point(p.x, p.y - 1), bi)
@@ -75,9 +75,9 @@ object MapColoring {
     while (!q.isEmpty) {
       val p = q.dequeue()
       val color = bi.getRGB(p.x, p.y)
-      if (color == target) {
+      if (color == targetColor) {
         processed.add(p)
-        bi.setRGB(p.x, p.y, yellowRGB)
+        bi.setRGB(p.x, p.y, replacementColor)
         val left: Point = new Point(p.x - 1, p.y)
         if (!processed.contains(left)) q.enqueue(left)
         val right: Point = new Point(p.x + 1, p.y)
@@ -87,6 +87,36 @@ object MapColoring {
         val down: Point = new Point(p.x, p.y + 1)
         if (!processed.contains(down)) q.enqueue(down)
       }
+    }
+  }
+
+  /**
+    * Flood fill using a loop for the west and east directions as an optimization to avoid the overhead of stack or queue management
+    *
+    * https://en.wikipedia.org/wiki/Flood_fill
+    */
+  def floodFillQueueEastWest(initial: Point, bi: BufferedImage) {
+    val q = scala.collection.mutable.Queue[Point]()
+    q.enqueue(initial)
+    while (!q.isEmpty) {
+      val p = q.dequeue()
+      var minX = p.x
+      var keepGoing = true
+      while (keepGoing) {
+        if (bi.getRGB(minX, p.y) != targetColor) keepGoing = false
+        else minX = minX - 1
+      }
+      var maxX = p.x
+      keepGoing = true
+      while (keepGoing) {
+        if (bi.getRGB(maxX, p.y) != targetColor) keepGoing = false
+        else maxX = maxX + 1
+      }
+      (minX to maxX).foreach(x => {
+        bi.setRGB(x, p.y, replacementColor)
+        if (bi.getRGB(x, p.y - 1) == targetColor) q.enqueue(new Point(x, p.y - 1))
+        if (bi.getRGB(x, p.y + 1) == targetColor) q.enqueue(new Point(x, p.y + 1))
+      })
     }
   }
 }
